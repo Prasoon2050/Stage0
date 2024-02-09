03ed058facf06147f131d969d3c5a5a09d91f172
surajarya0007 <aryasuraj351@gmail.com>
2024-02-09 21:08:33 +0530

    Added Header and Footer

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <p>Hero Header Section</p>
+      <p>Features Section</p>
+      <p>Features list section</p>
+      <p>Ecommerce Product list Section</p>
+      <p>Testimonial Section</p>
+      <p>CTA Section</p>
+      <p>Newsletter Section</p>
+      <p>Contact Section</p>
+    </div>
+  );
+}
